d3a3683b1d09034b73a45579588b0e37d7c49b77
David Havera <djhavera@yahoo.com>
2019-08-29 09:12:10 -0400

    Add files via upload

---
+++ configs/config-drizzle.js
@@ -0,0 +1,20 @@
+import AmazingDapp from "./contracts/AmazingDapp.json";
+
+const drizzleOptions = {
+  web3: {
+    block: false,
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:7545'
+    }
+  },
+  contracts: [AmazingDapp],
+  events: {
+    AmazingDapp: ["TaskCreated"],
+  },
+  polls: {
+    accounts: 1500 /* 1.5 seconds polling for account changes in MetaMask */
+  }
+}
+
+export default drizzleOptions
