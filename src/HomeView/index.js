f521dabfdc21a75715ca35340c613c14c33076dc
David Havera <djhavera@yahoo.com>
2019-08-29 09:11:06 -0400

    Add files via upload

---
+++ src/HomeView/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import SetString from "./SetString";
+
+import { DrizzleContext, DrizzleProvider } from "drizzle-react";
+class HomeView extends Component {
+  render() {
+    return (
+      <div>
+      <div className="container">Welcome to React DApp Boilerplate with Drizzle!</div>
+     
+      </div>
+    )
+  }
+}
+
+export default HomeView
